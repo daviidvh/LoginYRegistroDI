f110fd9aaefa7e7aa685218b11170ed0170680c1
daviidvh <davvel833@alu.centroafuera.es>
2023-01-28 12:02:33 +0100

    Falta parte programacion usuario

--- app/src/main/java/com/example/task08guiloginyregistro/pantallaRegistro.kt
+++ app/src/main/java/com/example/task08guiloginyregistro/pantallaRegistro.kt
@@ -1,11 +1,29 @@
 package com.example.task08guiloginyregistro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task08guiloginyregistro.databinding.ActivityPantallaRegistroBinding
 
 class pantallaRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla_registro)
+        val binding = ActivityPantallaRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgBotonCerrar.setOnClickListener(){
+            finish()
+        }
+
+        binding.botonCancelar.setOnClickListener {
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.botonRegistrar.setOnClickListener {
+            val intent= Intent(this, pantallaRegistro::class.java)
+            startActivity(intent)
+        }
+
     }
 }
